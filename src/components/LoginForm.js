d9597ae859b8425f6450174214109f68530b055a
saher <aldobaisaher@gmail.com>
2026-08-09 15:36:55 +0300

    Add bootstrap package and fix styles

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,7 +12,7 @@ export default function Login({ onLogin }) {
     setError('');
 
     try {
-      const response = await fetch(' https://system-backend-h8kw.onrender.com/api/auth/login', {
+      const response = await fetch(' https://system-backend-rwsk.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
